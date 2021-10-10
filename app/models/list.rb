590c33a445ef00f08a6c1eaa970a3f580f81cd22
EC2 Default User <ec2-user@ip-172-31-95-73.ec2.internal>
2021-10-10 06:02:48 +0000

    chapter3-9

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
 class List < ApplicationRecord
   attachment :image
 
-  validates :title, presence: ture
-  validates :body, presence: ture
-  validates :image, presence: ture
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
